3db570ef061f192d568067f0e1dd9da66aa35e4a
Alfred Gomes <alfredgomes23@users.noreply.github.com>
2023-09-18 18:04:13 +0600

    basic navbar done.

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,14 @@
 
 import './App.css'
+// import DaisyNavbar from './components/DaisyNavbar/DaisyNavbar'
+import MyNavbar from './components/MyNavbar/MyNavbar'
 
 function App() {
 
   return (
     <div>
-      <h1 className='text-5xl bg-rose-500'>Vite + React</h1>
-      <div className="hero min-h-screen bg-base-200">
-        <div className="hero-content text-center">
-          <div className="max-w-md">
-            <h1 className="text-5xl font-bold">Hello there</h1>
-            <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-            <button className="btn btn-primary">Get Started</button>
-          </div>
-        </div>
-      </div>
+            {/* <DaisyNavbar></DaisyNavbar> */}
+            <MyNavbar></MyNavbar>
     </div>
   )
 }
