6dc069bcca92e94f7b849412927d1532ee18514e
Alfred Gomes <alfredgomes23@users.noreply.github.com>
2023-09-18 17:11:56 +0600

    reconnect project.

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <h1 className='text-5xl bg-rose-500'>Vite + React</h1>
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content text-center">
+          <div className="max-w-md">
+            <h1 className="text-5xl font-bold">Hello there</h1>
+            <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+            <button className="btn btn-primary">Get Started</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
